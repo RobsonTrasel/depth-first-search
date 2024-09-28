a3b8b469e515d7e716baeadd7d1f015377c2369c
RobsonTrasel <robson.trasel@gmail.com>
2024-09-28 00:26:50 -0300

    chore: run some on main class

--- src/main/java/com/robsonf/App.java
+++ src/main/java/com/robsonf/App.java
@@ -1,7 +1,29 @@
 package com.robsonf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Vertex v1 = new Vertex("A");
+        Vertex v2 = new Vertex("B");
+        Vertex v3 = new Vertex("C");
+        Vertex v4 = new Vertex("D");
+        Vertex v5 = new Vertex("E");
+
+        List<Vertex> list = new ArrayList<>();
+        v1.addNeighbor(v2);
+        v1.addNeighbor(v3);
+        v3.addNeighbor(v4);
+        v4.addNeighbor(v5);
+
+        list.add(v1);
+        list.add(v2);
+        list.add(v3);
+        list.add(v4);
+        list.add(v4);
+
+        DepthFirstSearch dfs = new DepthFirstSearch();
+        dfs.dfs(list);
     }
 }
