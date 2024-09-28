71206d4828b48a1d67c61b2640118cb45e12c521
RobsonTrasel <robson.trasel@gmail.com>
2024-09-28 00:17:05 -0300

    chore: create base vertex class

--- src/main/java/com/robsonf/Vertex.java
+++ src/main/java/com/robsonf/Vertex.java
@@ -1,5 +1,37 @@
 package com.robsonf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Vertex {
+    private String name;
+    private boolean visited;
+    private List<Vertex> adjacencyList;
+
+    public Vertex(String name) {
+        this.name = name;
+        this.adjacencyList = new ArrayList<>();
+    }
+
+    public void addNeighbor(Vertex v) {
+        adjacencyList.add(v);
+    }
+
+    public List<Vertex> getNeighbors() {
+        return adjacencyList;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
